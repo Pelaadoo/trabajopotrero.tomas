4d4178661b4b252e72240ba3b0fde84df9ac7efe
Pelaadoo <133899501+Pelaadoo@users.noreply.github.com>
2023-11-02 13:08:53 -0300

    Add files via upload

---
+++ trabajopotrero.php
@@ -0,0 +1,88 @@
+<?php
+    echo "<h1>Hola Mundo!</h1>";
+
+    echo "<br>";
+
+
+    $variable = 5;
+    echo $variable;
+    echo "<br>";
+    $Variable = "3";
+    echo $variable;
+    echo "<br>";
+    echo $Variable;
+    echo "<br>";
+
+    var_dump($variable);
+    echo "<br>";
+    $variable = "4";
+    var_dump($variable);
+    echo "<br>";
+
+
+    $numero1 = 4;
+    $numero2 = 10;
+
+
+    echo $numero1 + $numero2;
+    // echo 5 + 10;
+    // echo 15; => 15
+    echo "<br>";
+    //RESTA
+    echo $numero1 - $numero2;
+    echo "<br>";
+    //MULTIPLICACION
+    echo $numero1 * $numero2;
+    echo "<br>";
+    //DIVISION
+    echo $numero2 / $numero1;
+    echo "<br>";
+    //RESTO
+    echo $numero2 % $numero1;
+
+
+    echo $numero2 - $numero1
+
+    echo "¡Hola, mundo!";
+
+$nombre = "Juan";
+$edad = 25;
+echo "Mi nombre es $nombre y tengo $edad años.";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
